5f550abb658f216ab63646491600d8e34843faa7
59114_Chetana <deslechetna1999@gmail.com>
2022-05-05 20:48:39 +0530

    [Chetana] Added : Creat Binary Search Tree as shown in given figure and calculate size

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -9,7 +9,18 @@ class Program
         MyBinaryTree<int> binarysearch = new MyBinaryTree<int>(56);
         binarysearch.Insert(30);
         binarysearch.Insert(70);
+        binarysearch.Insert(22);
+        binarysearch.Insert(40);
+;       binarysearch.Insert(60);
+        binarysearch.Insert(95);
+        binarysearch.Insert(11);
+        binarysearch.Insert(65);
+        binarysearch.Insert(3);
+        binarysearch.Insert(16);
+        binarysearch.Insert(63);
+        binarysearch.Insert(67);
         binarysearch.Display();
+        binarysearch.GetSize();
 
     }
 }
